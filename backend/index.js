d04ac5eff768ce3bd1bdcc64f641878a588078c1
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-02-21 07:50:30 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,17 @@
 //BASE PREP
 const express = require("express");
 const uuid = require("uuid");
-const path = require("path");
-const binDir = path.join(__dirname, "bins/");
-const binDirPath = path.resolve(
-  "C:UserskorenDocumentsGitHubpre-course-2021-final-boilerplate\backend\bins"
-);
-let tasks = require("./Tasks");
 const fs = require("fs");
-const { error } = require("console");
-
+const path = require("path");
 const app = express();
 
+app.use(function (req, res, next) {
+  setTimeout(next, 1000);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "../src")));
+app.use(express.static(path.join(__dirname, "../src"), { etag: false }));
+
 //BASE END
 
 //ROUTES
@@ -31,11 +28,11 @@ app.get("/all", (req, res) => {
       listofTasks.push(file);
     });
     if (listofTasks.length < 1) {
-      return res.status(400).json({
+      return res.status(404).json({
         msg: `No bins found`,
       });
     } else {
-      res.send(`Bins available: \n${listofTasks.join("\n")}`);
+      res.status(200).send(listofTasks);
     }
   });
 });
@@ -46,7 +43,7 @@ app.get("/b/:id", (req, res) => {
     if (!data) {
       res.status(400).json(`No bin found by the id of ${req.params.id}`);
     } else {
-      res.send(JSON.stringify(JSON.parse(data), null, 2));
+      res.status(200).send(JSON.stringify(JSON.parse(data), null, 2));
     }
   });
 });
@@ -56,20 +53,32 @@ app.post("/", (req, res) => {
   const binID = uuid.v4();
   let obj = { record: [] };
   let json = JSON.stringify(obj, null, 2);
-  fs.writeFile(`backend/bins/${binID}.json`, `${json}`, "utf8", () => {
-    res.json(`${binID}`);
-  });
+  try {
+    fs.writeFile(`backend/bins/${binID}.json`, `${json}`, "utf8", () => {
+      res.json(`${binID}`);
+    });
+  } catch {
+    res.status(400).send(`ERROR!, ${err}`);
+  }
 });
 
 //on PUT request: update the bin according to it's id
-app.put("/b/:id", (req, res) => {
-  const BIN_ID = req.params.id;
-  let obj = { record: [] };
-  obj.record.push(req.body);
-  let json = JSON.stringify(obj, null, 2);
-  fs.writeFile(`backend/bins/${BIN_ID}.json`, json, "utf8", (data) => {
-    res.send(`bin updated. ${json}`);
-  });
+app.put("/b/:id", (req, res, next) => {
+  try {
+    const BIN_ID = req.params.id;
+    let obj = { record: [] };
+    obj.record.push(req.body);
+    let json = JSON.stringify(obj, null, 2);
+    fs.writeFile(`backend/bins/${BIN_ID}.json`, json, "utf8", (data) => {
+      res.status(201).send(`bin updated. ${json}`);
+    });
+  } catch {
+    res.status(404).json({
+      statusCode: 404,
+      error: true,
+      msg: `File ${BIN_ID} not found`,
+    });
+  }
 });
 
 //on DELETE request: delete the specified bin
@@ -83,26 +92,6 @@ app.delete("/b/:id", (req, res) => {
     console.log(err);
   }
 });
-
-//////////////////////////////////////////////////
-//TASK-SPECIFIC ROUTES (After sign-in) (can be used for things other than "Todolist")
-//////////////////////////////////////////////////
-
-//on GET request: if the specified ID exists, show appropriate task
-
-//on DELETE request: delete the specified task
-// app.delete("/b/:id", (req, res) => {
-//   const found = userBin.some((task) => task.id == req.params.id);
-//   if (found) {
-//     const index = userBin.findIndex(
-//       (task) => task.id === parseInt(req.params.id)
-//     );
-//     userBin.splice(index, 1);
-//     res.json({ msg: "task deleted", userBin });
-//   } else {
-//     res.status(400).json({ msg: `No task with the id of ${req.params.id}` });
-//   }
-// });
 
 //ROUTES END
 
